96dcfc94a290ecbf4b500826e77daf59273a1909
varletint <intvarlet@hotmail.com>
2026-03-28 01:43:28 +0100

    feat(app): configure routing and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { ROUTES } from '@/config/routes';
 import { RegisterPage } from '@/features/auth/pages/RegisterPage';
+import { LoginPage } from '@/features/auth/pages/LoginPage';
 import { HomePage } from '@/features/home/pages/HomePage';
 import { ProductsPage } from '@/features/products/pages/ProductsPage';
 import { ProductDetailsPage } from '@/features/products/pages/ProductDetailsPage';
+import { CartPage } from '@/features/cart/pages/CartPage';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
 import { SEO } from '@/components/SEO';
@@ -22,7 +24,8 @@ function App() {
           <Route path={ROUTES.HOME} element={<HomePage />} />
           <Route path={ROUTES.PRODUCTS} element={<ProductsPage />} />
           <Route path="/products/:slug" element={<ProductDetailsPage />} />
-          <Route path={ROUTES.LOGIN} element={<div>Login (Coming Soon)</div>} />
+          <Route path={ROUTES.CART} element={<CartPage />} />
+          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
           <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
