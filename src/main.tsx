0716c7b8d2fa0445bbc2f824a3daad305b69ec06
varletint <intvarlet@hotmail.com>
2026-04-05 03:58:01 +0100

    refactor(providers): wrap App in AppProviders component

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AppProviders } from './providers/AppProviders';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'sonner';
 import './index.css';
 import App from './App.tsx';
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <AppProviders>
       <HelmetProvider>
         <BrowserRouter>
           <App />
           <Toaster position="top-center" richColors theme="system" />
         </BrowserRouter>
       </HelmetProvider>
-    </QueryClientProvider>
+    </AppProviders>
   </StrictMode>
 );
