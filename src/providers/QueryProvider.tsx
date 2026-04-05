111c9415cf6096feb589e30812d9470c7fbd48ee
varletint <intvarlet@hotmail.com>
2026-04-05 03:58:08 +0100

    chore(providers): apply formatting to QueryProvider

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,6 +1,6 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { queryClient } from "@/lib/react-query";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '@/lib/react-query';
 
 interface QueryProviderProps {
   children: React.ReactNode;
